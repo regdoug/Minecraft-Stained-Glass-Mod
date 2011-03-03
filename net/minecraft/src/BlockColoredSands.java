287b1bb85247aec5ed8a382d80390d6eb7916a75
Reginald Pierce <pierce.reginald@gmail.com>
2011-03-03 15:03:54 -0500

    Colored sands ready for testing?

--- net/minecraft/src/BlockColoredSands.java
+++ net/minecraft/src/BlockColoredSands.java
@@ -1,29 +1,63 @@
 package net.minecraft.src;
 
-public class BlockColoredSands extends Block {
+import java.util.HashMap;
 
-    public BlockColoredSands(int )
+public class BlockColoredSands extends BlockSand {
 
-    public static Block black;
-    public static Block red;
-    public static Block green;
-    public static Block brown;
-    public static Block blue;
-    public static Block purple;
-    public static Block cyan;
-    //silver is the color of regular Sand
-    public static Block gray;
-    public static Block pink;
-    public static Block lime;
-    public static Block yellow;
-    public static Block lightBlue;
-    public static Block magenta;
-    public static Block orange;
-    public static Block white;
+    public BlockColoredSands(int i, int j){
+        super(i,j);
+    }
 
-    static {
-        black = new
+    @Override
+    public int getBlockTextureFromSideAndMetadata(int i, int j){
+        Integer a = spriteIDs.get(new Integer(j));
+        if(a == null){
+            return blockIndexInTexture;
+        }else{
+            return a.intValue();
+        }
     }
     
+    /*
+     * white    - 0x0
+     * orange   - 0x1
+     * magenta  - 0x2
+     * ltBlue   - 0x3
+     * yellow   - 0x4
+     * lime     - 0x5
+     * pink     - 0x6
+     * gray     - 0x7
+     * 
+     *   light gray (or silver) is the "color" of regular sand
+     * 
+     * cyan     - 0x9
+     * purple   - 0xa
+     * blue     - 0xb
+     * brown    - 0xc
+     * green    - 0xd
+     * red      - 0xe
+     * black    - 0xf
+     */
+
+    public static final HashMap<Integer,Integer> spriteIDs;
+
+    static {
+        spriteIDs = new java.util.HashMap<Integer, Integer>();
+        spriteIDs.put(new Integer(0x0),ModLoader.addOverride("/terrain.png","/path/to/sand_white.png"));
+        spriteIDs.put(new Integer(0x1),ModLoader.addOverride("/terrain.png","/path/to/sand_orange.png"));
+        spriteIDs.put(new Integer(0x2),ModLoader.addOverride("/terrain.png","/path/to/sand_magenta.png"));
+        spriteIDs.put(new Integer(0x3),ModLoader.addOverride("/terrain.png","/path/to/sand_ltBlue.png"));
+        spriteIDs.put(new Integer(0x4),ModLoader.addOverride("/terrain.png","/path/to/sand_yellow.png"));
+        spriteIDs.put(new Integer(0x5),ModLoader.addOverride("/terrain.png","/path/to/sand_lime.png"));
+        spriteIDs.put(new Integer(0x6),ModLoader.addOverride("/terrain.png","/path/to/sand_pink.png"));
+        spriteIDs.put(new Integer(0x7),ModLoader.addOverride("/terrain.png","/path/to/sand_gray.png"));
+        spriteIDs.put(new Integer(0x9),ModLoader.addOverride("/terrain.png","/path/to/sand_cyan.png"));
+        spriteIDs.put(new Integer(0xa),ModLoader.addOverride("/terrain.png","/path/to/sand_purple.png"));
+        spriteIDs.put(new Integer(0xb),ModLoader.addOverride("/terrain.png","/path/to/sand_blue.png"));
+        spriteIDs.put(new Integer(0xc),ModLoader.addOverride("/terrain.png","/path/to/sand_brown.png"));
+        spriteIDs.put(new Integer(0xd),ModLoader.addOverride("/terrain.png","/path/to/sand_green.png"));
+        spriteIDs.put(new Integer(0xe),ModLoader.addOverride("/terrain.png","/path/to/sand_red.png"));
+        spriteIDs.put(new Integer(0xf),ModLoader.addOverride("/terrain.png","/path/to/sand_black.png"));
+    }
 
 }
